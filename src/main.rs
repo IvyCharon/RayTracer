67b52fdea6a99c7977a025fac9bc53753fc96369
IvyCharon <shihanwen@sjtu.edu.cn>
2020-07-24 11:18:22 +0800

    add randomm

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,24 @@ use hittable::Hittable_list;
 use image::{ImageBuffer, RgbImage};
 use indicatif::ProgressBar;
 use ray::Ray;
+extern crate rand;
 
 const PI:f64 = 3.1415926535897932385;
 const INFINITY: f64 = 1e15;
 
 pub use vec3::Vec3;
 
-fn ray_color(r: Ray, world: &Hittable_list) -> Vec3 {
+fn ray_color(r: Ray, world: &Hittable_list, depth: i32) -> Vec3 {
     let rec = world.hit(r, 0.0, INFINITY);
+    if depth <= 0{
+        return Vec3::new(0.0,0.0,0.0);
+    }
     match rec {
-        Option::Some(rec) => (rec.normal + Vec3::ones()) * 0.5,
+        Option::Some(rec) =>{
+            //(rec.normal + Vec3::ones()) * 0.5
+            let target:Vec3 = rec.p + rec.normal + Vec3::random_in_unit_sphere();
+            return ray_color(Ray::new(rec.p, target - rec.p), world, depth - 1) * 0.5;
+            }
         Option::None => {
             let t = 0.5 * (r.dir.unit().y + 1.0);
             Vec3 {
@@ -44,17 +52,24 @@ fn main() {
     let horizontal = Vec3::new(16.0 / 9.0 * 2.0, 0.0, 0.0);
     let vertical = Vec3::new(0.0, 2.0, 0.0);
     let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, 1.0);
+    let samples_per_pixel = 100;
+    let max_depth = 50;
+
     for j in 0..225 {
         for i in 0..400 {
+            let mut col = Vec3::new(0.0,0.0,0.0);
+            for s in 0..samples_per_pixel.clone(){
+                let u = (i as f64 + rand::random::<f64>()) / (400.0 - 1.0);
+                let v = (225.0 - j as f64 + rand::random::<f64>()) / (225.0 - 1.0);
+                let r = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
+                col += ray_color(r, &world, max_depth.clone());
+            }
             let pixel = img.get_pixel_mut(i, j);
-            let u = i as f64 / (400.0 - 1.0);
-            let v = (225 - j) as f64 / (225.0 - 1.0);
-            let r = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
-            let color = ray_color(r, &world);
+            
             *pixel = image::Rgb([
-                (color.x * 255.999) as u8,
-                (color.y * 255.999) as u8,
-                (color.z * 255.999) as u8,
+                (col.x * 255.999/(samples_per_pixel.clone()as f64)) as u8,
+                (col.y * 255.999/(samples_per_pixel.clone()as f64)) as u8,
+                (col.z * 255.999/(samples_per_pixel.clone()as f64)) as u8,
             ]);
         }
         bar.inc(1);
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,5 +1,12 @@
 use std::ops::{Add, AddAssign};
 
+//use crate::rand;
+
+use rand::prelude::*;
+
+extern crate rand;
+use rand::Rng;
+
 #[derive(Clone, Debug, PartialEq, Copy)]
 pub struct Vec3 {
     pub x: f64,
@@ -211,6 +218,34 @@ impl Vec3 {
             x: self.x / a,
             y: self.y / a,
             z: self.z / a,
+        }
+    }
+}
+
+impl Vec3 {
+    pub fn Random() -> Vec3{
+        Vec3::new(
+            rand::random::<f64>(),
+            rand::random::<f64>(),
+            rand::random::<f64>()
+        )
+    }
+
+    pub fn Random_(mi: f64, ma: f64) -> Vec3{
+        let mut rng = rand::thread_rng();
+        Vec3::new(
+            rng.gen_range(mi, ma),
+            rng.gen_range(mi, ma),
+            rng.gen_range(mi, ma)
+        )
+    }
+
+    pub fn random_in_unit_sphere() -> Vec3 {
+        loop {
+            let p = Vec3::Random_(-1.0,1.0);
+            if p.length_squared() < 1.0 {
+                return p;
+            }
         }
     }
 }
