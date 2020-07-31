f0a32d5aeba0c305beb23817dfc2b634edec1a0c
IvyCharon <shihanwen@sjtu.edu.cn>
2020-07-31 18:03:26 +0800

    night(bigger one)

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,7 @@ fn clamp(x: f64, min: f64, max: f64) -> f64 {
 }
 
 fn main() {
-    let samples_per_pixel = 400;
+    let samples_per_pixel = 500;
     let max_depth = 50;
 
     let choose = 1;
@@ -89,7 +89,7 @@ fn main() {
             //night
             world = HittableList::night();
             aspect_ratio = 3.0 / 2.0;
-            image_width = 1200;
+            image_width = 1600;
             image_height = ((image_width as f64) / aspect_ratio) as u32;
 
             lookfrom = Vec3::new(13.0, 2.0, 3.0);
